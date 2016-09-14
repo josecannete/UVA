649d746371997661984e9cab1fb907100cb1da1d
José Cañete <jose.canete@ug.uchile.cl>
2016-09-13 21:08:03 -0300

    Más problemas

--- justfinish.cpp
+++ justfinish.cpp
@@ -9,34 +9,34 @@ int main(){
 	int t;
 	cin >> t;
 	int asd = 0;
-	
+
 	while(t--){
 		asd++;
 		int n;
 		cin >> n;
-		vector<ll> uno(n+1,0), dos(n+1,0), tres(n+1,0);
-		ll a;
-		for (int i = 1; i <= n; i++){
-			cin >> a;
-			uno[i] = uno[i-1] + a;
+		vector<ll> uno(2*n,0), dos(2*n,0), tres(2*n,0);
+		ll suma = 0;
+		for (int i = 0; i < n; i++)
+			cin >> uno[i], suma += uno[i];
+		for (int i = 0; i < n; i++)
+			cin >> dos[i], suma -= dos[i];
+		if (suma < 0) { cout << "Case " << asd << ": Not possible" << endl; continue; }
+		for (int i = 0; i < n; i++){
+			uno[i+n] = uno[i];
+			dos[i+n] = dos[i];
 		}
-		for (int i = 1; i <= n; i++){
-			cin >> a;
-			dos[i] = dos[i-1] + a;
+		suma = 0;
+		int marcador = 0;
+		for (int i = 0; i < 2*n; i++){
+			suma += (uno[i] - dos[i]);
+			if (suma < 0){
+				suma = 0;
+				marcador = i;
+			}
+			else{
+				tres[i] = marcador;
+			}
 		}
-		for (int i = 1; i <= n; i++)
-			tres[i] = uno[i] - dos[i];
-			
-		if (tres[n] < 0)
-			cout << "Case " << asd << ": Not possible" << endl;
-		else{
-			int i = 1;
-			int ultimo = 0;
-			for (; i <= n; i++)
-				if (tres[i] < 0){
-					ultimo = i;
-				}
-			cout << "Case " << asd << ": Possible from station " << (ultimo + 1) % n << endl;
-		}		
+		cout << "Case " << asd << ": Possible from station " <<  tres[n] + 2 << endl;
 	}
 }
--- poleposition.cpp
+++ poleposition.cpp
@@ -6,19 +6,45 @@ using namespace std;
 int main(){
 	int n;
 	cin >> n;
-	
+
 	while( n != 0 ){
-		vector<int> vec(n+1,0);
+		vector<int> vec(n+1,0), pos(n+1,0), fin(n+1,0);
 		int contador = 0;
 		int suma = 0;
-		for (int i = 0; i < n; i++){
-			int c, p;
-			cin >> c >> p;
-			suma += p;
-			if (p != 0) contador++;
+		for (int i = 1; i <= n; i++){
+			cin >> vec[i] >> pos[i];
+			suma += pos[i];
+			if (suma != 0) contador++;
 		}
-		if (suma == 0 && contador % 2 == 0)
-			cout <<
-	} 
+		//cout << suma << endl;
+		if (suma != 0) {cout << -1 << endl;}
+		else{
+			for (int i = 1; i <= n; i++){
+				fin[i + pos[i] ] = vec[i];
+			}
+			vector<int> fin2(n+1,0);
+			fin2 = fin;
+			sort(vec.begin(), vec.end());
+			sort(fin2.begin(), fin2.end());
+			int flag = 0;
+			for (int i = 0; i <= n; i++){
+				//cout << vec[i] << "     " << fin2[i] << endl;
+				if (vec[i] != fin2[i]){
+					//cout << vec[i] << "   " << fin2[i] << endl;
+					cout << -1 << endl;
+					flag = 1;
+					break;
+				}
+			}
+			if (!flag){
+			for (int i = 1; i <= n; i++){
+				cout << fin[i];
+				if (i != n) cout << " ";
+			}
+			cout << endl;
+		}
+		}
+		cin >> n;
+	}
 
 }
