8fb40abdccb1114ef575c1d75cdc839885fd776b
José Cañete <jose.canete@ug.uchile.cl>
2016-09-22 01:31:07 -0300

    Más problemas

--- fillthecontainers.cpp
+++ fillthecontainers.cpp
@@ -6,7 +6,38 @@ using namespace std;
 int main(){
 	int n, m;
 	while (cin >> n >> m){
-		
-		
+		vector<int> vasels(n+1,0);
+		for (int i = 0; i < n; i++)
+			cin >> vasels[i];
+		bool sepuede;
+
+		int fin = 1e9, ini = 0, med;
+		while (fin - ini > 1){
+			sepuede = 1;
+			med = (ini + fin) / 2;
+			int cont = 1;
+			int cantidad = 0;
+			for (int i = 0; i < n; i++){
+				if (vasels[i] > med){
+					sepuede = 0;
+					break;
+				}
+				else{
+					if (cantidad + vasels[i] > med){
+						cantidad = 0;
+						cont++;
+					}
+					if (cont > m){
+						sepuede = 0;
+						break;
+					}
+					cantidad += vasels[i];
+				}
+			}
+			if (sepuede) fin = med;
+			else ini = med;
+		}
+		cout << fin << endl;
+
 	}
 }
