365c5cf0e59766a019b20149ff2baebc13b7fd0e
José Cañete <jose.canete@ug.uchile.cl>
2016-09-21 21:05:31 -0300

    Más problemas

--- distributingballotboxes.cpp
+++ distributingballotboxes.cpp
@@ -2,33 +2,33 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#define ii pair<int,int>
 
 int main(){
   std::ios::sync_with_stdio(false);
   int n, b;
   cin >> n >> b;
   while (n != -1 && b != -1){
-    map<int,ii> mapa;
-    priority_queue<ii> cola;
-    for (int i = 0; i < n; i++){
-      int a; cin >> a;
-      mapa[i] = ii(a, 1);
-      cola.push(ii(a, i));
-    }
-    b -= n;
-    for (int i = 0; i < b; i++){
-      ii tope = cola.top(); cola.pop();
-      ii enmapa = mapa[tope.second];
-      //cout << enmapa.first << "   "  << enmapa.second << endl;
-      //cout << " asd es  " << enmapa.first / (enmapa.second+1) << endl;
-      mapa[tope.second] = ii(enmapa.first, enmapa.second + 1);
-      cola.push(ii(enmapa.first / (enmapa.second+1), tope.second));
-    }
-    ii ultimo = cola.top();
-    cout << ultimo.first << endl;
-
-
-    cin >> n >> b;
+	  vector<int> personas(n);
+	  int ini=0, fin=0, med=0; 
+	  for (int i = 0; i < n; i++){
+		cin >> personas[i];
+		fin = max(fin,personas[i]);
+	}
+	int cont = 0;
+	while (fin > ini){
+		cont = 0;
+		med = (fin + ini) / 2;
+		for (int i = 0; i < n; i++){
+			cont += (personas[i] + med - 1) / med;
+		}
+		if (cont <= b)
+			fin = med;
+		else ini = med+1;
+	}
+	cout << (fin+ini)/2 << endl;
+	  
+	  
+	  cin >> n >> b;
   }
+  
 }
---
+++ fillthecontainers.cpp
@@ -0,0 +1,12 @@
+// 11413 - Fill the Containers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n, m;
+	while (cin >> n >> m){
+		
+		
+	}
+}
